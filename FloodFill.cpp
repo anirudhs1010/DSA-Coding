a26a42fc6bc7961eab356e8d4b20b47e6fd79fad
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-08-28 09:27:50 -0500

    Create FloodFill.cpp

---
+++ FloodFill.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool visited[51][51];
+    int c = 0;
+    int y = 0;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if (c == 0) {
+            y = image[sr][sc];
+
+        }
+        if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || visited[sr][sc])
+            return image;
+        else if (image[sr][sc] == color || image[sr][sc] != y) {
+            return image;
+        }
+        
+        image[sr][sc] = color;
+        visited[sr][sc] = true;
+        c++;
+        
+        floodFill(image, sr-1, sc, color);
+        floodFill(image, sr+1, sc, color);
+        floodFill(image, sr, sc-1, color);
+        floodFill(image, sr, sc+1, color);
+        return image;
+    }
+};
