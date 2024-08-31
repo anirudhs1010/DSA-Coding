b61df09fe2469adff6879b7c5e856442d7987cbe
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-08-30 19:09:03 -0500

    Create ReachNextLevel.cpp

---
+++ ReachNextLevel.cpp
@@ -0,0 +1,4 @@
+//https://app.codesignal.com/arcade/code-arcade/at-the-crossroads/m9wjpkCjgofg7gs8N/
+bool solution(int experience, int threshold, int reward) {
+    return (reward+experience)>=threshold;
+}
