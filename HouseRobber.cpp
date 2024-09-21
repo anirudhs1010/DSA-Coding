fd9aa5afaba7827698ca3c599391746f35de2ace
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-09-20 20:46:29 -0500

    Update HouseRobber.cpp

--- HouseRobber.cpp
+++ HouseRobber.cpp
@@ -9,6 +9,6 @@ public:
         for (int i = 2; i < nums.size(); i++) {
             dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
         }
-        return dp[nums.size()-1];
+        return dp[dp.size()-1];
     }
 };
