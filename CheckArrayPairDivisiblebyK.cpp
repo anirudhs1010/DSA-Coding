48954d7028ce7857395753a8df13a6c76d238e3d
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-10-02 13:26:46 -0500

    Create CheckArrayPairDivisiblebyK.cpp

---
+++ CheckArrayPairDivisiblebyK.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        map<int, int> m;
+        if (arr.size()%2 != 0)
+            return false;
+        
+        //(b + (a % b)) % b
+        for (int i = 0; i < arr.size(); i++) {
+            m[(k + arr[i] % k)%k]++;
+        } 
+        if (m[0]%2 != 0)
+            return false;
+        for (const auto b : m) {
+            if (b.first != 0 && b.second != m[k-b.first]) {
+                return false;
+            }
+            
+        }
+        
+        return true;
+    }
+};
