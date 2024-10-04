6959ae8df3be5af35378501ab19bd063f7953da9
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-10-03 23:58:29 -0500

    Create MinStack.cpp

---
+++ MinStack.cpp
@@ -0,0 +1,41 @@
+class MinStack {
+public:
+    int min = INT_MAX;
+    map<int, int> m;
+    stack<int> s;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s.push(val);
+        m[val]++;
+    }
+    
+    void pop() {
+        int v = s.top();
+        s.pop();
+        m[v]--;
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        if (!m.empty()) {
+            const auto b = m.begin();
+            return b->first;
+        }
+        return 0;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
