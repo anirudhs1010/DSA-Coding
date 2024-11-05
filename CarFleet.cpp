239ae9e6bb359748375db3fb04fde80ff4f38eca
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-11-04 23:31:05 -0600

    Update CarFleet.cpp

--- CarFleet.cpp
+++ CarFleet.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        stack<int> s;
+        vector<pair<int, int>> m;
+        for (int i = 0; i < position.size(); i++) m.push_back({position[i], speed[i]});
+        sort(m.rbegin(), m.rend());
+        vector<double> s;
         for (int i = 0; i < position.size(); i++) {
-            int t = (target-position[i])/speed[i];
-            while (!s.empty() && t >= s.top())
-                s.pop();
-            s.push(t);
+            double t = (double)(target-m[i].first)/m[i].second;
+            s.push_back(t);
+            if (s.size() >= 2 && t <= s[s.size()-2])
+                s.pop_back();
         }
-       
         return s.size();
     }
 };
