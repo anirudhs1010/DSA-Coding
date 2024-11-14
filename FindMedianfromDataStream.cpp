e2104a12187951894cc907a466b12506b2188107
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-11-13 23:53:58 -0600

    Create FindMedianfromDataStream.cpp

---
+++ FindMedianfromDataStream.cpp
@@ -0,0 +1,22 @@
+//did not know the trick to make it O(1) rip well I guess I get it know ur just jumping between 2 heaps.
+class MedianFinder {
+public:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    MedianFinder() {
+    }
+    void addNum(int num) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    double findMedian() {
+        if (maxHeap.size() > minHeap.size()) return maxHeap.top();
+        return (maxHeap.top() + minHeap.top()) / 2.0;
+    }
+};
