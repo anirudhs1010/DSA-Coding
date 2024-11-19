07b132217ced8b273beda4bcf74ee75f8611cbe1
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-11-18 23:54:27 -0600

    Update UniquePaths.cpp

--- UniquePaths.cpp
+++ UniquePaths.cpp
@@ -6,7 +6,6 @@ public:
         for (int i = 1; i < dp.size(); i++) {
             for (int j = 1; j < dp[i].size(); j++) {
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
-         
             }
         }
         return dp[m-1][n-1];
