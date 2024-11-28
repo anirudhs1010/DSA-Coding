707c75a60484e1bf66c4477f65d83d9bbbe0beb8
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-11-27 23:59:54 -0800

    Update Range Sum Query - Immutable.cpp

--- Range Sum Query - Immutable.cpp
+++ Range Sum Query - Immutable.cpp
@@ -4,7 +4,6 @@ public:
     NumArray(vector<int>& nums) {
         v = nums;
     }
-    
     int sumRange(int left, int right) {
         int sum = 0;
         for (int i = left; i <= right; i++) {
