7300643cf07f9cca35f05f85c7de9c6711efc3f9
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-12-09 23:54:53 -0600

    Create WordBreak.cpp

---
+++ WordBreak.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    map<int, bool> m;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        m[s.size()] = true;
+        return dfs(s, wordDict, 0);
+    }
+
+private:
+    bool dfs(const string& s, const vector<string>& wordDict, int i) {
+        if (m.find(i) != m.end())
+            return m[i];
+        if (i == s.length()) {
+            return true;
+        }
+
+        for (const string& w : wordDict) {
+            if (i + w.length() <= s.length() && 
+                s.substr(i, w.length()) == w) {
+                m[i] = true;
+                return dfs(s, wordDict, i + w.length());
+            }
+        }
+        m[i] = false;
+        return false;
+    }
+};
