a63f3abddba508f44ce5fb6bde27376f3736d94d
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2024-12-30 23:04:49 -0800

    Update NumberOfConnectedComponents.cpp

--- NumberOfConnectedComponents.cpp
+++ NumberOfConnectedComponents.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
+    set<int> s;
+    map<int, vector<int>> m;
+
     int count = 0;
-    map<int, bool> m; //visited
-    map<int, vector<int>> nodes; //all the nodes
-    set<int> s; //in case we get a cycle
-    void dfs(int i, int parent) {
-        if (s.find(i) != s.end() && !m[i])
-            return;
+    bool dfs(int i) {
+        if (s.count(i))
+            return false;
         s.insert(i);
-        m[i] = true;
-        for (int h : nodes[i]) {
-            if (h != parent && !m[h])
-                dfs(h, i);
-            
+        vector<int> a = m[i];
+        for (int h: m[i]) {
+            dfs(h);
         }
+        return true;
     }
     int countComponents(int n, vector<vector<int>>& edges) {
-        for (int i = 0; i < n; i++) {
-            nodes[i] = {}; 
-            m[i] = false;
+        for (int i = 0; i <= n; i++) {
+            m[i] = {};
         }
         for (int i = 0; i < edges.size(); i++) {
             vector<int> v = edges[i];
-            nodes[v[0]].push_back(v[1]);
-            nodes[v[1]].push_back(v[0]);
+            m[v[0]].push_back(v[1]);
+            m[v[1]].push_back(v[0]); 
         }
-        
-        for (int i = 0; i < n; i++) {
-            if (!m[i]) { 
-                dfs(i, -1);
+        for (int i = 0; i < n; i++)
+            if (dfs(i))
                 count++;
-            }
-            
-        }
         return count;
-
     }
 };
