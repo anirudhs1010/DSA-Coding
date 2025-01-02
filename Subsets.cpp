c8f8bfd42e0f8a6e9f63173d7ef86ab8b3cff2c2
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2025-01-01 22:23:25 -0800

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> s;
-    void dfs(int index, vector<int> n) {
-        if (index >= n.size()) {
-            ans.push_back(s);
+    vector<vector<int>> P;
+    vector<int> v;
+    void backtrack(int s, vector<int> a) {
+        if (s == a.size()) {
+            P.push_back(v);
             return;
         }
-        s.push_back(n[index]);
-        dfs(index+1, n);
-        s.pop_back();
-        dfs(index+1, n);
-
+        v.push_back(a[s]);
+        backtrack(s+1, a);
+        v.pop_back();
+        backtrack(s+1, a);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        dfs(0, nums);
-
-        return ans;
+        backtrack(0, nums);
+        return P;
     }
 };
