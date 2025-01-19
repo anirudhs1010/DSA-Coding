9257da990e2317858331c03590f1ca40df0c0dfc
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2025-01-18 19:12:41 -0800

    Update LongestSubstringWithoutRepeatingChar.cpp

--- LongestSubstringWithoutRepeatingChar.cpp
+++ LongestSubstringWithoutRepeatingChar.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> a;
-        int mx = 0;
+        
         int l = 0, r = 0;
+        unordered_set<int> b;
+        int maxs = 0;
         while (r < s.size()) {
-            while (a.find(s[r]) != a.end()) {
-                a.erase(s[l]);
-                l++;
-            }
             
-            a.insert(s[r]);
-            mx = max((r-l)+1, mx);
+            while (b.find(s[r]) != b.end()) {
+                b.erase(s[l]);
+                l++;
+                
+            }
+            b.insert(s[r]);
             r++;
+            maxs = max(maxs, r-l);
         }
- 
-        return mx;
+        return maxs;
     }
 };
