3e7e22df08c60e7c192afd59672bb9f55024feb1
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2025-01-20 20:39:44 -0600

    Update PalindromicSubstrings.cpp

--- PalindromicSubstrings.cpp
+++ PalindromicSubstrings.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-    
     int countSubstrings(string s) {
-        if (s.size() == 0 || s.size() == 1)
-            return s.size();
-        string ans = "";
         int maxs = 0;
-        int count = 0;
         for (int i = 0; i < s.size(); i++) {
-            //even
+            int count = 0;
             int l = i, r = i;
-            while (l >= 0 && r <= s.size()-1 && s[l] == s[r]) {
-                if ((r - l + 1) > maxs) {
-                    maxs = r - l + 1;
-                    ans = s.substr(l, maxs);
-                    
-                }
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
+                count++;
+                l--;
+                r++;
+            }
+            maxs += count;
+            count = 0;
+            l = i, r = i+1;
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
                 l--;
                 r++;
                 count++;
             }
-            //odd
-            l = i;
-            r = i+1;
-            while (l >= 0 && r <= s.size()-1 && s[l] == s[r]) {
-                if ((r - l + 1) > maxs) {
-                    maxs = r - l + 1;
-                    ans = s.substr(l, maxs);
-                    
-                }
-                l--;
-                r++;
-                count++;
-            }
-            
+            maxs += count;
         }
-        return count;
-        
+       
+        return maxs;
     }
 };
