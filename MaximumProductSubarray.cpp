87566b30de0b0f7fadb9853bf75f63c0c2eed7f8
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2025-01-29 23:10:47 -0600

    Update MaximumProductSubarray.cpp

--- MaximumProductSubarray.cpp
+++ MaximumProductSubarray.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-       long long a = 1, n = INT_MIN;
-       for (int i = 0; i < nums.size(); i++) {
-            a *= nums[i];
-            n = max(n, a);
-            if (a == 0)
-                a = 1;
-       }
-       a = 1;
-       for (int i = nums.size()-1; i >= 0; i--) {
-            a *= nums[i];
-            n = max(n, a);
-            if (a == 0)
-                a = 1;
-       } 
-       return n;
-           
+        int a = INT_MIN, b = INT_MIN;
+        int maxP = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            maxP *= nums[i];
+            a = max(a, maxP);
+            if (maxP == 0)
+                maxP = 1;
+        }
+        int maxN = 1;
+        for (int i = nums.size()-1; i >= 0; i--) {
+            maxN *= nums[i];
+            a = max(a, maxN);
+            if (maxP == 0)
+                maxP = 1;
+        }
+        return max(a, b);
     }
 };
