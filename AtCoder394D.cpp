b6cc9a6ed4bb58c4fc309fc74cf35fe23950bcaf
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2025-02-23 17:00:08 -0600

    Create AtCoder394D.cpp

---
+++ AtCoder394D.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> g;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(' || s[i] == '<' || s[i] == '[')
+            g.push(s[i]);
+        else if (!g.empty()) {
+            if (g.top() == '(' && s[i] == ')')
+                g.pop();
+            else if (g.top() == '[' && s[i] == ']')
+                g.pop();
+            else if (g.top() == '<' && s[i] == '>')
+                g.pop();
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    return g.empty();
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    if (isValid(s))
+        cout << "Yes" <<endl;
+    else
+        cout<<"No";
+
+    return 0;
+}
