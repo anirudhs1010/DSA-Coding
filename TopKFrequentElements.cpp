61225da75aac255a731ef381fb46dce30d7b6c97
Anirudh Sunil <56770976+anirudhs1010@users.noreply.github.com>
2025-03-13 23:58:55 -0500

    Update TopKFrequentElements.cpp

--- TopKFrequentElements.cpp
+++ TopKFrequentElements.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         map<int, int> m;
-        priority_queue<pair<int, int>> q;
         for (int i = 0; i < nums.size(); i++) {
             m[nums[i]]++;
         }
-        for (const auto b:m ) {
+        priority_queue<pair<int, int>> q;
+        for (const auto b : m ) 
             q.push({b.second, b.first});
-        }
-        vector<int> res;
-        while (k > 0) {
+        vector<int> v;
+        while (k--) {
             pair<int, int> p = q.top();
-            res.push_back(p.second);
+            v.push_back(p.second);
             q.pop();
-            k--;
         }
-        return res;
 
+        return v;
     }
 };
